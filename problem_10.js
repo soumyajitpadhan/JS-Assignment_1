86279ab58ad593091d5af99f1b7e498c287a8d8d
Soumyajit Padhan <soumyajitpadhan15@gmail.com>
2024-10-22 11:25:18 +0530

    problem-10

---
+++ problem_10.js
@@ -0,0 +1,9 @@
+let price = 100;
+let discountPercent = 10;
+
+let discountAmount = (price * discountPercent) / 100;
+
+let finalPrice = price - discountAmount;
+
+console.log("The final price after discount is: " + finalPrice);
+
